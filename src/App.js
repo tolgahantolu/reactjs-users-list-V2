45d92fd8ce6631ae0a9e104026f9f2165434bd9c
Tolgahan Tolu <tolgahanntolu@gmail.com>
2022-05-20 11:34:51 +0300

    UserList ve ErrorModal componenti oluşturuldu stil verildi kullanıcılar listelendi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uEmail) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, mail: uEmail, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+
+      <UsersList users={usersList} />
     </>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
 function AddUser(props) {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [error, setError] = useState();
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
+
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredEmail.trim().length === 0
+    ) {
+      setError({
+        title: "Invalid Value",
+        message: "Please enter a valid name and email (non-empty values).",
+      });
+      return;
+    }
+
+    //console.log(enteredUsername, enteredEmail);
+    props.onAddUser(enteredUsername, enteredEmail); //!App.js'den geliyor
+
+    setEnteredUsername("");
+    setEnteredEmail("");
+  };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const emailChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
     <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler} //ErrorModal.js'e gönderiyoruz!
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={onSubmitHandler}>
           <label htmlFor="username">Username: </label>
-          <input id="username" type="text" />
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
 
           <label htmlFor="email">Mail Adress: </label>
-          <input id="email" type="email" />
+          <input
+            id="email"
+            type="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+          />
 
           <Button type="submit">Add User</Button>
         </form>
