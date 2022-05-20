b3f7ec88aa8c31dc9b8eaab923c7c21e85f78bd7
Tolgahan Tolu <tolgahanntolu@gmail.com>
2022-05-20 16:40:14 +0300

    ErrorModal button bugı duzeltildi

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -5,7 +5,13 @@ import classes from "./Button.module.css";
 function Button(props) {
   return (
     <>
-      <button className={classes.button}>{props.children}</button>
+      <button
+        className={classes.button}
+        type={props.type || "button"}
+        onClick={props.onClick}
+      >
+        {props.children}
+      </button>
     </>
   );
 }
